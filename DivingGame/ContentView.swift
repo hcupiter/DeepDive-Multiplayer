a0e73756cfb3eaa150c4475767b888c0fda8a820
Hans Arthur Cupiterson <KetsunoAna117@users.noreply.github.com>
2024-04-25 14:30:09 +0700

    Initial Commit

---
+++ DivingGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DivingGame
+//
+//  Created by Hans Arthur Cupiterson on 25/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DivingGame/DivingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivingGameApp.swift
+//  DivingGame
+//
+//  Created by Hans Arthur Cupiterson on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DivingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
